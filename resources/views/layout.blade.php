f44e0792852721cb31aa61f388c7abf4ac8bc33a
André Ribas <andreribas@gmail.com>
2019-07-19 09:37:16 -0300

    Add a little spacing between posts in the index page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=<?=filemtime('css/app.css')?>">
 
     <title>@yield('title', 'blog')</title>
 </head>
